c2846415dc70f9150b933532e69816a2cc161e01
jamiekim828 <jamiekim828@gmail.com>
2023-03-14 18:10:50 +0100

    front route setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,40 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
+
 import './App.css';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Books from './pages/Books';
+import BookDetail from './components/Books/BookDetail';
+import MyAchievement from './components/User/MyAchievement';
+import UserInformation from './components/User/UserInformation';
+import BookShelves from './pages/BookShelves';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import MyBooks from './pages/MyBooks';
+import OneBookShelf from './components/BookShelves/OneBookShelf';
 
 function App() {
   return (
-    <div className="App">
-      Hello group 3! 
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/books' element={<Books />} />
+          <Route path='/books/:bookId' element={<BookDetail />} />
+          <Route path='/:userId/books' element={<MyBooks />} />
+          <Route path='/:userId/achievment' element={<MyAchievement />} />
+          <Route path='/:userId/setting' element={<UserInformation />} />
+          <Route path='/bookshelves/all' element={<BookShelves />} />
+          <Route
+            path='/bookshelves/:bookshelvesId'
+            element={<OneBookShelf />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Books.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BooksList from '../components/Books/BooksList';
+
+
+export default function Books() {
+  return (
+    <div>
+        <BooksList />
+    </div>
+  );
+}
