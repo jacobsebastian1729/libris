100a69a94447b3b69c8a849893655b21d711344e
Antoine <antoine.aubard@gmail.com>
2023-03-15 21:12:59 +0100

    fixed type in one file

--- client/src/types/type.ts
+++ client/src/types/type.ts
@@ -1,19 +1,37 @@
-export type User = {
-
+export type UserType = {
+    fullName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
 }
 
-export type Book = {
-    title: string,
-    thumbnail: string,
-    language: string,
+export type UserDataType = {
+    _id: string;
+    email: string;
+    fullName: string;
+    profile_img?: string;
+    about_me?: string;
+    isAdmin: boolean;
+    status: string;
+    followers: string[];
+    following: string[];
+    bookShelves: string[];
+    // comments: string[];
+}
+
+export type BookType = {
+    title: string;
+    thumbnail: string;
+    language: string;
     description: string;
     rating: number;
     genre: string;
     author: string;
-}
+    _id: string;
+  };
 
-export type BookList = Book & {
-    favorite: string
+export type BookList = BookType & {
+    bookShelf: string
 }
 
 export type BookShelf = {
