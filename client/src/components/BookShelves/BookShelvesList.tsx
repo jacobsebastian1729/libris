565586f45d5de010e38e196c8a57ae6d356ad808
Antoine <antoine.aubard@gmail.com>
2023-03-15 20:50:14 +0100

    fixed conflict

--- client/src/components/BookShelves/BookShelvesList.tsx
+++ client/src/components/BookShelves/BookShelvesList.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { RootState, AppDispatch } from "../../redux/store"
+import { getBookShelfList } from "../../redux/thunk/bookShelf";
 
 export default function BookShelvesList() {
+  const userInformation = useSelector(
+    (state: RootState) => state.user.userInformation
+  );
+  const userId = userInformation._id;
+  const bookShelfList = useSelector((state: RootState) => state.bookShelf.bookShelfList);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(getBookShelfList(userId));
+  }, [dispatch, userId])
+
+  console.log(bookShelfList, "bookShelfList")
   return (
     <div>BookShelvesList</div>
   )
---
+++ client/src/redux/thunk/bookShelf.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { AppDispatch } from "../store";
+
+import { bookShelfListActions } from "../slices/bookShelf";
+
+const url = "http://localhost:8000/bookshelves/";
+
+export function getBookShelfList(userId: string) {
+    return async (dispatch: AppDispatch) => {
+        const userToken = localStorage.getItem("userToken");
+        const res = await axios.get(`${url}/${userId}`, {
+            headers: { Authorization: `Bearer $ {userToken}`},
+        });
+        const bookShelfData = res.data;
+        dispatch(bookShelfListActions.getBookShelfList(bookShelfData));
+    };
+};
