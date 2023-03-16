427e68abf9f0c8a4a7fe6ff8970ca31a99293806
jamiekim828 <jamiekim828@gmail.com>
2023-03-16 12:02:27 +0100

    basic dashboard for user and books are done

--- client/src/components/DashBoard/BooksBoard.tsx
+++ client/src/components/DashBoard/BooksBoard.tsx
@@ -1,7 +1,98 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import TableContainer from '@mui/material/TableContainer';
+import TableBody from '@mui/material/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@mui/material/Button';
+import { TableCell } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Table from '@mui/material/Table';
+import Rating from '@mui/material/Rating';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+
+import { AppDispatch, RootState } from '../../redux/store';
+import { getAllUserData } from '../../redux/thunk/user';
+import { Typography } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { fetchbookData } from '../../redux/thunk/book';
+
+function createData(
+  Id: string,
+  Title: string,
+  Author: string,
+  Rating: number
+  //   Status: string,
+  //   Role: string,
+  //   Books: [],
+  //   Following: [],
+  //   Followers: [],
+  //   Comments: []
+) {
+  return {
+    Id,
+    Title,
+    Author,
+    Rating,
+  };
+}
+
+const rows = [];
 
 export default function BooksBoard() {
+  const allBooks = useSelector((state: RootState) => state.bookItem.Book);
+
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(fetchbookData());
+  }, []);
+
   return (
-    <div>BooksBoard</div>
-  )
+    <div>
+      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+        <Typography variant='h2'>Books Management</Typography>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'right',
+            paddingRight: '1rem',
+            marginBottom: '1rem',
+          }}
+        >
+          <LibraryAddIcon />
+          <Typography>Add More Books</Typography>
+        </div>
+      </div>
+      <TableContainer
+        component={Paper}
+        sx={{ marginLeft: '.5rem', paddingRight: '.5rem' }}
+      >
+        <Table aria-label='simple table' stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell align='left'>Title</TableCell>
+              <TableCell align='left'>Author</TableCell>
+              <TableCell align='center'>Rating</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {allBooks.map((row) => (
+              <TableRow key={row._id}>
+                <TableCell component='th' scope='row'>
+                  {row._id}
+                </TableCell>
+                <TableCell align='left'>{row.title}</TableCell>
+                <TableCell align='left'>{row.author}</TableCell>
+                <TableCell align='center'>
+                  <Rating name='read-only' value={row.rating} readOnly />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
--- client/src/components/DashBoard/DashBoard.tsx
+++ client/src/components/DashBoard/DashBoard.tsx
@@ -5,59 +5,62 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-
 import './DashBoard.css';
 import { Link } from 'react-router-dom';
 
 export default function DashBoard() {
-  
   return (
-    <div className='dashboard'>
-      <Card
-        sx={{ width: 300, maxWidth: 345, marginRight: '1rem' }}
-        className='user-board'
-      >
-        <CardMedia
-          sx={{ height: 140 }}
-          image='/static/images/cards/contemplative-reptile.jpg'
-          title='green iguana'
-        />
-        <CardContent>
-          <Typography gutterBottom variant='h5' component='div'>
-            Users
-          </Typography>
-          <Typography variant='body2' color='text.secondary'>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link to='/dashboard/users'>
-            <Button size='small'>User Management</Button>
-          </Link>
-        </CardActions>
-      </Card>
-      <Card sx={{ width: 300, maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image='/static/images/cards/contemplative-reptile.jpg'
-          title='green iguana'
-        />
-        <CardContent>
-          <Typography gutterBottom variant='h5' component='div'>
-            Books
-          </Typography>
-          <Typography variant='body2' color='text.secondary'>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link to='/dashboard/books'>
-            <Button size='small'>Book Management</Button>
-          </Link>
-        </CardActions>
-      </Card>
+    <div>
+      <div style={{textAlign: 'center', marginTop: '2rem'}}>
+        <Typography variant='h2'>Welcome to Dashboard</Typography>
+      </div>
+      <div className='dashboard'>
+        <Card
+          sx={{ width: 300, maxWidth: 345, marginRight: '1rem', height: '50%' }}
+          className='user-board'
+        >
+          <CardMedia
+            sx={{ height: 140 }}
+            image='/static/images/cards/contemplative-reptile.jpg'
+            title='green iguana'
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+              Users
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to='/dashboard/users' style={{textDecoration: 'none'}}>
+              <Button size='small'>User Management</Button>
+            </Link>
+          </CardActions>
+        </Card>
+        <Card sx={{ width: 300, maxWidth: 345, height: '50%'  }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image='/static/images/cards/contemplative-reptile.jpg'
+            title='green iguana'
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+              Books
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to='/dashboard/books' style={{textDecoration: 'none'}}>
+              <Button size='small'>Book Management</Button>
+            </Link>
+          </CardActions>
+        </Card>
+      </div>
     </div>
   );
 }
