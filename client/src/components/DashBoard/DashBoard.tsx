863eb8a551302897778323eab6588c95397373f6
jamiekim828 <jamiekim828@gmail.com>
2023-03-16 10:38:44 +0100

    dash board card direct to user board and book board

--- client/src/components/DashBoard/DashBoard.tsx
+++ client/src/components/DashBoard/DashBoard.tsx
@@ -1,6 +1,3 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -9,64 +6,58 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 
-import { AppDispatch, RootState } from '../../redux/store';
-import { getAllUserData } from '../../redux/thunk/user';
-import './DashBoard.css'
-import Table from './Table';
+import './DashBoard.css';
+import { Link } from 'react-router-dom';
 
 export default function DashBoard() {
-  const allUsers = useSelector((state: RootState) => state.user.users);
-//   const allBooks = useSelector((state: RootState) => state.)
-
-
-
-  const dispatch = useDispatch<AppDispatch>();
-  useEffect(() => {
-    dispatch(getAllUserData());
-  }, []);
-
-  console.log('dashboard',allUsers)
-  return (<div className='dashboard'>
-    <Card sx={{ maxWidth: 345 }} className='user-board'>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Users
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Books
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  </div>
- )
+  
+  return (
+    <div className='dashboard'>
+      <Card
+        sx={{ width: 300, maxWidth: 345, marginRight: '1rem' }}
+        className='user-board'
+      >
+        <CardMedia
+          sx={{ height: 140 }}
+          image='/static/images/cards/contemplative-reptile.jpg'
+          title='green iguana'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            Users
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Link to='/dashboard/users'>
+            <Button size='small'>User Management</Button>
+          </Link>
+        </CardActions>
+      </Card>
+      <Card sx={{ width: 300, maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image='/static/images/cards/contemplative-reptile.jpg'
+          title='green iguana'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            Books
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Link to='/dashboard/books'>
+            <Button size='small'>Book Management</Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
